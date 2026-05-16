dc98d43ed290eff2b5f48fbfde7fc0abf14655f4
Eduardo Rossatto <erp.rossatto@gmail.com>
2026-05-16 16:32:50 -0300

    alteração 2

--- hello.py
+++ hello.py
@@ -5,7 +5,8 @@ modificação = 'Utilizando o  vscode'
 print(modificação) 
 mensagem_completa = mensagem + ' ' + modificação
 print(mensagem_completa)    
-
+mai = 'alô mundo!' \
+'print(mai)'
 
 
 
--- mensagem.py
+++ mensagem.py
@@ -2,3 +2,5 @@
 mensagem ='hello world! Utilizando o git no vscode' 
 print(mensagem)
 
+novo_projeto = 'Criando um novo projeto no vscode'
+print(novo_projeto)
