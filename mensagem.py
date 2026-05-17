d3c94a083ad93e2dec48b69a6be97456351909a5
Eduardo Rossatto <erp.rossatto@gmail.com>
2026-05-17 14:19:05 -0300

    comitando no mensagem.py

--- mensagem.py
+++ mensagem.py
@@ -4,3 +4,7 @@ print(mensagem)
 
 novo_projeto = 'Criando um novo projeto no vscode'
 print(novo_projeto)
+
+nova_mensagem = 'Essa é uma nova mensagem no mensagem.py para testar o git'
+print(nova_mensagem)   
+
