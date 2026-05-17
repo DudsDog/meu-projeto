f854c4f7840eecd935fd9230838140e54cf59d7b
Eduardo Rossatto <erp.rossatto@gmail.com>
2026-05-17 19:18:04 -0300

    ALTERANDO TUDO

--- hello.py
+++
@@ -1,9 +0,0 @@
-MAIS_UMA_MUDANÇA = 'Mais uma mudança para testar o git'
-print(MAIS_UMA_MUDANÇA)
-
-MUDANÇA_FINAL = 'Essa é a última mudança para testar o git'
-print(MUDANÇA_FINAL)
-
-
-VAMOS_DE_NOVO = 'Vamos de novo para testar o git'
-print(VAMOS_DE_NOVO)
--- mensagem.py
+++ mensagem.py
@@ -1,10 +1,4 @@
 
-mensagem ='hello world! Utilizando o git no vscode' 
-print(mensagem)
-
-novo_projeto = 'Criando um novo projeto no vscode'
-print(novo_projeto)
-
-nova_mensagem = 'Essa é uma nova mensagem no mensagem.py para testar o git'
-print(nova_mensagem)   
+mensagem ='NOVA FEATURE'
+PRINT(mensagem)
 
